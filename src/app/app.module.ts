d8627997e11792735dc3ac2e3058e59d65861451
Igor-Garnik <garnik_igor@ukr.net>
2018-07-27 09:30:55 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { DriversComponent } from './drivers/drivers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    DriversComponent
   ],
   imports: [
     BrowserModule
